332312acf26431c498358d376c972dd366f75770
Alex <AL97EX@users.noreply.github.com>
2020-03-16 23:52:11 +0200

    Start

---
+++ Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _enemySpeed;
+    [SerializeField] private float _tempEnemySpeed;
+    [SerializeField] private float _timeStopEnemy;
+    [SerializeField] private float _timeGoEnemy;
+
+    [SerializeField] private Transform _leftPoint;
+    [SerializeField] private Transform _rightPoint;
+
+    [SerializeField] private float _moveDirection;
+    [SerializeField] private Rigidbody2D _rigitbody;
+    
+    private Vector2 moveEnemy;
+
+    private Vector2 _leftPointVector;
+    private Vector2 _rightPointVector;
+    private float _distanceLeft;
+    private float _distanceRight;
+    void Start()
+    {
+        _rigitbody = GetComponent<Rigidbody2D>();
+        _tempEnemySpeed = _enemySpeed;
+        _timeStopEnemy = Random.Range(5f, 10f);
+    }
+
+    void Update()
+    {
+        Distance();
+        EnemyMove();
+        EnemyDelay();
+    }
+    private void FixedUpdate()
+    {
+        _rigitbody.MovePosition(_rigitbody.position + moveEnemy * Time.fixedDeltaTime);
+    }
+    private void EnemyMove()
+    {
+        moveEnemy = Vector2.right * _moveDirection * _enemySpeed;
+        if (_distanceLeft<=0.1f)
+        {
+            _moveDirection = 1;
+        }
+        else if (_distanceRight <= 0.1f)
+        {
+            _moveDirection = -1;
+        }
+    }
+    private void Distance()
+    {
+        _leftPointVector = transform.position - _leftPoint.position;
+        _rightPointVector = transform.position - _rightPoint.position;
+        _distanceLeft = Mathf.Abs(Mathf.Sqrt(_leftPointVector.x * _leftPointVector.x + _leftPointVector.y * _leftPointVector.y));
+        _distanceRight = Mathf.Abs(Mathf.Sqrt(_rightPointVector.x * _rightPointVector.x + _rightPointVector.y * _rightPointVector.y));
+    }
+    private void EnemyDelay()
+    {
+        if (_timeStopEnemy <= 0)
+        {
+            _enemySpeed = 0;
+            if (_timeGoEnemy <= 0)
+            {
+                _enemySpeed = _tempEnemySpeed;
+                _timeStopEnemy = _timeGoEnemy = Random.Range(5f, 10f);
+            }
+            _timeGoEnemy -= .1f;
+        }
+        _timeStopEnemy -= .1f;
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float gravity=-10;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance=0.1f;
+    [SerializeField] private float heightJump;
+
+    [SerializeField] private Transform cam;
+    [SerializeField] private float camZoom;
+    [SerializeField] private Transform parent;
+    [SerializeField] private Transform bulletPref;
+    [SerializeField] private Transform bulletPos;
+    [SerializeField] private EnergyFull energyFull;
+
+    [SerializeField] private int dust;
+    [SerializeField] private GameObject speedMagic;
+    [SerializeField] private Transform speedMagicPlace;
+
+    [SerializeField] private Transform explosionPlace;
+    [SerializeField] GameObject explosion;
+
+    private GameObject cloneExplosion;
+    private bool isGround;
+    private GameObject cloneSpeedMagic;
+    private Vector2 moveHorizontal;
+    private Vector2 velocity;
+
+    public List<Transform> lifeList;
+    public int speed = 10;
+    public int moveSide { get; set; }
+    void Start()
+    {
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            lifeList.Add(parent.GetChild(i));
+        }
+    }
+
+    void Update()
+    {
+        //isGround Check
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGround && velocity.y < 0)
+        {
+            velocity.y = -0.5f;
+        }
+        if (isGround && dust == 0)
+        {
+            cloneExplosion = Instantiate(explosion, explosionPlace.position, Quaternion.identity);
+            Destroy(cloneExplosion, 1.5f);
+            dust++;
+        }
+
+        //Fire
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPref, bulletPos.position, Quaternion.LookRotation(-bulletPos.right));
+        }
+        if (moveSide==-1)
+        {
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
+        else if (moveSide == 1)
+        {
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        moveHorizontal = Vector2.right * moveSide * speed * Time.deltaTime;
+        controller.Move(moveHorizontal);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+
+    private void LateUpdate()
+    {
+        cam.position = new Vector3(transform.position.x, transform.position.y + 3, camZoom);
+    }
+    void Running()
+    {
+        if (energyFull.enabled)
+        {
+            speed = 15;
+            cloneSpeedMagic = Instantiate(speedMagic, speedMagicPlace.position, Quaternion.identity);
+            Destroy(cloneSpeedMagic, 1.5f);
+        }
+    }
+    public void Jump()
+    {
+        if (isGround) {
+            dust = 0;
+            velocity.y = Mathf.Sqrt(heightJump * -2 * gravity);
+        }
+    }
+    public void Fire()
+    {
+        Instantiate(bulletPref, bulletPos.position, Quaternion.LookRotation(-bulletPos.right));
+    }
+
+    
+}
