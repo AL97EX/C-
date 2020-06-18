9bbbb1b90b0a23b4078d4717de5946a5ec0e696a
Alex <alex3ken@gmail.com>
2020-06-18 22:39:25 +0300

    PuzzleAdded

---
+++ Test/Casual/Puzzle/Assets/Scripts/AddButtons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButtons : MonoBehaviour
+{
+    [SerializeField] private RectTransform _puzzleField;
+    [SerializeField] private GameObject _puzzleButtonPrefab;
+
+    private GameObject _instantiateBtn;
+    void Awake()
+    {
+        for(int i = 0; i < 12; i++)
+        {
+            _instantiateBtn = Instantiate(_puzzleButtonPrefab);
+            _instantiateBtn.name = $"{i}";
+            _instantiateBtn.transform.SetParent(_puzzleField);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Test/Casual/Puzzle/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public List<Button> _puzzleBtns;
+    public Transform _puzzleField;
+    public Sprite _buttonSprite;
+    void Start()
+    {
+        for(int i = 0; i < _puzzleField.childCount; i++)
+        {
+            _puzzleBtns.Add(_puzzleField.GetChild(i).GetComponent<Button>());
+            _puzzleBtns[i].image.sprite = _buttonSprite;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
