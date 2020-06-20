1c32d52d18424c00509cc068981db70e2ae615c5
Alex <53299871+AL97EX@users.noreply.github.com>
2020-06-20 20:04:05 +0300

    Main Menu added

---
+++ Projects/Casual/Puzzle/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneIndex);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
